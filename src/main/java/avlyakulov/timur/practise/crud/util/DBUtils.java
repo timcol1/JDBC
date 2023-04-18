8cfb1ecd165f8048b00a5ed35d23682c0b7da3cb
timcol1 <avlyakulovtimur@gmail.com>
2023-04-18 15:43:45 +0300

    add implementation via config.properties

--- src/main/java/avlyakulov/timur/practise/crud/util/DBUtils.java
+++ src/main/java/avlyakulov/timur/practise/crud/util/DBUtils.java
@@ -1,19 +1,24 @@
 package avlyakulov.timur.practise.crud.util;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBUtils {
     //здесь бы будем создавать connection
-    private static String dbURL = "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'classpath:init.sql'";
+    //private static String dbURL = "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'classpath:init.sql'";
 
-    private static String dbUsername = "sa";
-    private static String dbPassword = "";
+    /*private static String dbUsername = "sa";
+    private static String dbPassword = "";*/
 
     public static Connection getConnection() {
         //пример взаимодействия с папкой resources для бд
-        /*String dbURL = null;
+        String dbURL = null;
         String dbUsername = "sa";
         String dbPassword = "";
 
@@ -25,7 +30,7 @@ public class DBUtils {
             e.printStackTrace();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }*/
+        }
 
         Connection connection = null;
         try {
