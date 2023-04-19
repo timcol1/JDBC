cfc2ad31796cf290ed5e3e9ed0df0d77a7d9feba
timcol1 <avlyakulovtimur@gmail.com>
2023-04-19 12:03:56 +0300

    add simple queries

--- src/main/java/avlyakulov/timur/practise/db_mysql/DBUtil.java
+++ src/main/java/avlyakulov/timur/practise/db_mysql/DBUtil.java
@@ -1,8 +1,7 @@
 package avlyakulov.timur.practise.db_mysql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class DBUtil {
     //connect with maven - driver in dependencies
@@ -10,8 +9,15 @@ public class DBUtil {
         String userName = "root";
         String password = "1504";
         String connectionUrl = "jdbc:mysql://localhost:3306/example";
-        try (Connection connection = DriverManager.getConnection(connectionUrl,userName,password)) {
-            System.out.println("We are connected");
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+             PreparedStatement preparedStatement = connection.prepareStatement("Select * from books")) {
+            //preparedStatement.executeUpdate(); // кодга мы хотим обновить базу данных
+            //preparedStatement.executeUpdate("Update books set name = '1984' where id = 2");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.printf("%d %s\n",resultSet.getInt("id"), resultSet.getString("name"));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
