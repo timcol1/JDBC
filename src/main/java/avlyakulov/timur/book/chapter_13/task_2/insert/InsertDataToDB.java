0766189482d090b6169e85e7e96fd0c88bee10b6
timcol1 <avlyakulovtimur@gmail.com>
2023-05-03 10:01:15 +0300

    Finish class InsertDataToDB

--- src/main/java/avlyakulov/timur/book/chapter_13/task_2/insert/InsertDataToDB.java
+++ src/main/java/avlyakulov/timur/book/chapter_13/task_2/insert/InsertDataToDB.java
@@ -13,43 +13,70 @@ public class InsertDataToDB {
 
     public void insertToTableData(BufferedReader reader) {
         try (Connection connection = ConnectToVideoLibraryDB.getConnectionToDB();
-             PreparedStatement insertActors = connection.prepareStatement(SQL_QUERY_INSERT_ACTORS);
-             PreparedStatement insertFilm = connection.prepareStatement(SQL_QUERY_INSERT_FILMS)) {
+             PreparedStatement queryActor = connection.prepareStatement(SQL_QUERY_INSERT_ACTORS);
+             PreparedStatement queryFilm = connection.prepareStatement(SQL_QUERY_INSERT_FILMS);
+             PreparedStatement queryDirector = connection.prepareStatement(SQL_QUERY_INSERT_DIRECTORS)) {
             System.out.println("Choose table where you want insert data \n1.actors\n2.directors \n3.films");
             String answer = reader.readLine();
             switch (answer) {
                 case "1" -> {
                     System.out.println("Enter the full name of actor");
                     String fullNameActor = reader.readLine();
-                    insertActors.setString(1, fullNameActor);
+                    queryActor.setString(1, fullNameActor);
                     System.out.println("Enter the date of birth actor in format yyyy-MM-dd");
                     String dateStr = reader.readLine();
                     Date date = Date.valueOf(dateStr); //converting string into sql date
-                    insertActors.setDate(2, date);
-                    System.out.println("Insert name of the film where this actor was filmed");
+                    queryActor.setDate(2, date);
+                    System.out.println("Insert name of the film from this list where this actor was filmed");
+                    Statement statement = connection.createStatement();
+                    ResultSet setOfFilms = statement.executeQuery("Select name_film from videolibrary.films");
+                    int i = 1;
+                    while (setOfFilms.next()) {
+                        System.out.printf("%d. %s \n", i, setOfFilms.getString(1));
+                        ++i;
+                    }
                     String nameFilm = reader.readLine();
-                    PreparedStatement findIdFilm = connection.prepareStatement("Select id_film from videolibrary.films where name_film = '" + nameFilm + "'");
-                    ResultSet resultSet = findIdFilm.executeQuery();
-                    resultSet.next();
-                    int idFilm = resultSet.getInt(1);
-                    insertActors.setInt(3, idFilm);
-                    insertActors.executeUpdate();
+                    ResultSet getIdFilm = statement.executeQuery("Select id_film from videolibrary.films where name_film = '" + nameFilm + "'");
+                    getIdFilm.next();
+                    int idFilm = getIdFilm.getInt(1);
+                    queryActor.setInt(3, idFilm);
+                    queryActor.executeUpdate();
                 }
                 case "2" -> {
-
+                    System.out.println("Enter the full name of director");
+                    String fullNameDirector = reader.readLine();
+                    queryDirector.setString(1, fullNameDirector);
+                    System.out.println("Enter the date of birth director in format yyyy-MM-dd");
+                    String dateStr = reader.readLine();
+                    Date date = Date.valueOf(dateStr); //converting string into sql date
+                    queryDirector.setDate(2, date);
+                    System.out.println("Enter the name of the film from this list which this director produced");
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery("Select name_film from videolibrary.films");
+                    int i = 1;
+                    while (resultSet.next()) {
+                        System.out.printf("%d. %s \n", i, resultSet.getString(1));
+                        ++i;
+                    }
+                    String nameFilm = reader.readLine();
+                    resultSet = statement.executeQuery("Select id_film from videolibrary.films where name_film = '" + nameFilm + "'");
+                    resultSet.next();
+                    int idFilm = resultSet.getInt(1);
+                    queryDirector.setInt(3, idFilm);
+                    queryDirector.executeUpdate();
                 }
                 case "3" -> {
                     System.out.println("Enter the name of film");
                     String nameFilm = reader.readLine();
-                    insertFilm.setString(1, nameFilm);
+                    queryFilm.setString(1, nameFilm);
                     System.out.println("Enter the date of film in format yyyy-MM-dd");
                     String dateStr = reader.readLine();
                     Date date = Date.valueOf(dateStr); //converting string into sql date
-                    insertFilm.setDate(2, date);
+                    queryFilm.setDate(2, date);
                     System.out.println("Enter the country of your film");
                     String country = reader.readLine();
-                    insertFilm.setString(3, country);
-                    insertFilm.executeUpdate();
+                    queryFilm.setString(3, country);
+                    queryFilm.executeUpdate();
                 }
             }
         } catch (SQLException | IOException e) {
